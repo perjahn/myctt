11b96397d3febf70a904b2700b06b855abcaea86
Per Jahn <per.jahn@collectorbank.se>
2019-04-20 11:53:26 +0200

    1st ver

---
+++ myctt/MyLogger.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Web.XmlTransform;
+using System;
+
+namespace myctt
+{
+    class MyLogger : IXmlTransformationLogger
+    {
+        public void EndSection(string message, params object[] messageArgs)
+        {
+            Log($"EndSection: {string.Format(message, messageArgs)}");
+        }
+
+        public void EndSection(MessageType type, string message, params object[] messageArgs)
+        {
+            Log($"EndSection ({type}): {string.Format(message, messageArgs)}");
+        }
+
+        public void LogError(string message, params object[] messageArgs)
+        {
+            Log($"Error: {string.Format(message, messageArgs)}", ConsoleColor.Red);
+        }
+
+        public void LogError(string file, string message, params object[] messageArgs)
+        {
+            Log($"Error: file: {file}, {string.Format(message, messageArgs)}", ConsoleColor.Red);
+        }
+
+        public void LogError(string file, int lineNumber, int linePosition, string message, params object[] messageArgs)
+        {
+            Log($"Error: file: {file} (line {lineNumber}, {linePosition}), {string.Format(message, messageArgs)}", ConsoleColor.Red);
+        }
+
+        public void LogErrorFromException(Exception ex)
+        {
+            Log($"ErrorFromException: ex: {ex.ToString()}", ConsoleColor.Red);
+        }
+
+        public void LogErrorFromException(Exception ex, string file)
+        {
+            Log($"ErrorFromException: ex: {ex.ToString()}{Environment.NewLine}File: {file}", ConsoleColor.Red);
+        }
+
+        public void LogErrorFromException(Exception ex, string file, int lineNumber, int linePosition)
+        {
+            Log($"ErrorFromException: ex: {ex.ToString()}{Environment.NewLine}File: {file} (line {lineNumber}, {linePosition})", ConsoleColor.Red);
+        }
+
+        public void LogMessage(string message, params object[] messageArgs)
+        {
+            Log($"Message: {string.Format(message, messageArgs)}");
+        }
+
+        public void LogMessage(MessageType type, string message, params object[] messageArgs)
+        {
+            Log($"Message ({type}): {string.Format(message, messageArgs)}");
+        }
+
+        public void LogWarning(string message, params object[] messageArgs)
+        {
+            Log($"Warning: {string.Format(message, messageArgs)}", ConsoleColor.Yellow);
+        }
+
+        public void LogWarning(string file, string message, params object[] messageArgs)
+        {
+            Log($"Warning: file: {file}, {string.Format(message, messageArgs)}", ConsoleColor.Yellow);
+        }
+
+        public void LogWarning(string file, int lineNumber, int linePosition, string message, params object[] messageArgs)
+        {
+            Log($"Warning: file: {file} (line {lineNumber}, {linePosition}), {string.Format(message, messageArgs)}", ConsoleColor.Yellow);
+        }
+
+        public void StartSection(string message, params object[] messageArgs)
+        {
+            Log("StartSection");
+        }
+
+        public void StartSection(MessageType type, string message, params object[] messageArgs)
+        {
+            Log($"StartSection ({type}): {string.Format(message, messageArgs)}");
+        }
+
+        void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        void Log(string message, ConsoleColor color)
+        {
+            var oldColor = Console.ForegroundColor;
+            try
+            {
+                Console.ForegroundColor = color;
+                Console.WriteLine(message);
+            }
+            finally
+            {
+                Console.ForegroundColor = oldColor;
+            }
+        }
+    }
+}
---
+++ myctt/Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Web.XmlTransform;
+using System;
+
+namespace myctt
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Usage: myctt <sourcefile> <transformfile> <outfile>");
+                return 1;
+            }
+
+            string sourcefile = args[0];
+            string transformfile = args[1];
+            string destinationfile = args[2];
+
+            if (Transform(sourcefile, transformfile, destinationfile))
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        private static bool Transform(string sourcefile, string transformfile, string destinationfile)
+        {
+            var logger = new MyLogger();
+
+            using (var xdoc = new XmlTransformableDocument())
+            {
+                xdoc.PreserveWhitespace = true;
+                xdoc.Load(sourcefile);
+
+                using (var xmlTransform = new XmlTransformation(transformfile, logger))
+                {
+                    if (!xmlTransform.Apply(xdoc))
+                    {
+                        return false;
+                    }
+                    xdoc.Save(destinationfile);
+                }
+            }
+
+            return true;
+        }
+    }
+}
